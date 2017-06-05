4fce93f2f4cea10f0d6d6712926e624e35aa001a
Matthew Foyle <matt@moltin.com>
2017-06-05 14:21:27 +0100

    add products

--- index.js
+++ index.js
@@ -1,3 +1,16 @@
 var config = require('./config');
 var moltin = require('./utils/moltin');
 var Algolia = require('./utils/algolia')
+
+var AddProducts = function() => {
+  moltin.GetProducts()
+
+  .then((products) => {
+    var productData = products.data
+    Algolia.AddToAlgolia(productData)
+  })
+
+  .catch((err) => {
+    console.log(err)
+  });
+};
